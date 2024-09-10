1527554f3bde83361d88e65f0e760c04c2f020da
1donghyuk <wondh7909@gmail.com>
2024-09-11 02:48:35 +0900

    Add files via upload
    Signed-off-by: 1donghyuk <wondh7909@gmail.com>

---
+++ login_main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head> 
+<meta charset="utf-8">
+<title>INFOTHERAPY</title>
+<link rel="stylesheet" type="text/css" href="testcss.css">
+<link rel="stylesheet" type="text/css" href="login.css">	
+<script type="text/javascript" src="login.js"></script>
+</head>
+<header>
+	<?php include("header.php")
+	 ?>
+</header>
